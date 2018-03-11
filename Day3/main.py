d7636907cb857a6006bd3df322dfc771e7417324
GMcMonagle <L00130611@student.lyit.ie>
2018-03-11 18:45:07 +0000

    day 3 part 1 solved

--- Day3/main.py
+++ Day3/main.py
@@ -1,10 +1,14 @@
+import csv, os
 class MyGrid:
     grid = [[1]]
     sequence = ['R', 'U', 'L', 'D']
+    origin = [0,0]
+    last_added = [0,0]
 
     def __init__(self, target):
         self.target = target
         self.add_values(target)
+        self.grid.reverse()
 
     def add_values(self, target):
         line_length = 1
@@ -12,11 +16,12 @@ class MyGrid:
         direction = 'R'
         y = 0
         i = 2
-        while(i < target):
+        while(i <= target):
             count = i
             while (i < (count + line_length)):
-                y = self.get_y_val(y, direction)
-                self.add_value(y, i, direction)
+                if(i <= target):
+                    y = self.get_y_val(y, direction)
+                    self.last_added = self.add_value(y, i, direction)
                 i+=1
             directionOb = self.get_next_direction(direction,y)
             y = directionOb[1]
@@ -27,20 +32,13 @@ class MyGrid:
             else:
                 increase_line_length = True
 
-    def add_values_in_direction(self, count, direction, line_length, y):
-        i = count
-        while(i <= (count+line_length)):
-            y = self.get_y_val(y, direction)
-            self.add_value(y, i, direction)
-            i = i+1
-        return y
-
     def get_next_direction(self, current_direction, y):
         if(current_direction == "R"):
             self.grid.append([])
         if (current_direction == "L"):
             y = y +1
             self.grid.insert(0,[])
+            self.origin[1] = self.origin[1]+1
         for index, item in enumerate(self.sequence):
             if(current_direction == item):
                 if(index == (len(self.sequence)-1)):
@@ -49,7 +47,7 @@ class MyGrid:
                     return [self.sequence[index+1],y]
 
     def add_value(self, y, val, direction):
-        xVal = None
+        self.make_x_adjustments(y,direction)
         if(direction == 'R' or direction == 'U'):
             self.grid[y].append(val)
             xVal = len(self.grid[y])-1
@@ -58,6 +56,10 @@ class MyGrid:
             xVal = 0
         return  [xVal, y]
 
+    def make_x_adjustments(self, y, direction):
+        if(y == self.origin[1]):
+            if(direction == 'L' or direction == 'D'):
+                self.origin[0] = self.origin[0]+1
 
     def get_y_val(self, current_y, direction):
         if(direction == 'U'):
@@ -69,12 +71,48 @@ class MyGrid:
     def __getattr__(self, grid):
         return self.grid
 
-
-    def printGrid(self):
+    def  print_grid(self, file_name):
         for index, item in enumerate(self.grid):
             print(item)
-gridOb = MyGrid(35)
-print(gridOb.printGrid())
+
+    def get_steps(self):
+        if(self.last_added[0] > self.origin[0]):
+            hor_steps = self.last_added[0] - self.origin[0]
+        else:
+            hor_steps = self.origin[0] - self.last_added[0]
+        if(self.last_added[1] > self.origin[1]):
+            ver_steps = self.last_added[1] - self.origin[1]
+        else:
+            ver_steps = self.origin[1] - self.last_added[1]
+        return ver_steps + hor_steps
+
+    def out_csv(self):
+        if(os._exists('outputs.csv')):
+            os.remove('outputs.csv')
+        with open('output.csv', 'w') as myfile:
+            self.grid.reverse()
+            for index, item in enumerate(self.grid):
+                if(len(item) > 0):
+                    print(item)
+                    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                    wr.writerow(item)
+
+    def __getattr__(self, origin):
+            return self.origin
+    def __getattr__(self, last_added):
+        return self.last_added
+
+
+gridOb = MyGrid(277678)
+#gridOb.out_csv()
+print(gridOb.get_steps())
+
+
+
+
+
+
+
 
 
 
