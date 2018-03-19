85cfcfae3565cd3409b367737dfdd27dfc1a2368
GMcMonagle <L00130611@student.lyit.ie>
2018-03-19 18:18:06 +0000

    day10 p2 done. That was weird

---
+++ Day10/part2/main.py
@@ -0,0 +1,22 @@
+from circular_list import circular_list
+input_string = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
+#input_string = "1,2,3"
+lengths = []
+for idx, num in enumerate(input_string):
+    lengths.append(ord(num))
+lengths.append(17)
+lengths.append(31)
+lengths.append(73)
+lengths.append(47)
+lengths.append(23)
+
+my_list = circular_list(256)
+my_list.perform_looped_hask_knot(lengths,64)
+dense_hash = my_list.create_dense_hash()
+my_string = ""
+for idx, den in enumerate(dense_hash):
+    var = str(hex(int(den)).rstrip("L").lstrip("0x") or "0")
+    if len(var) == 1:
+        var = "0"+var
+    my_string += var
+print(my_string)
